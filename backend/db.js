8bbe3961c104c3a9a1856b1375215e4e0e18397f
daksh chhetri <dakshftw01@gmail.com>
2024-05-11 11:26:53 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://admin:gPM4Ubiqm7r37oJc@cluster0.jgcuqyd.mongodb.net/to-do');
+mongoose.connect('');
 
 const todoSchema = mongoose.Schema ({
     title: String,
